bc8009d6bf30151b0abc252e1391bf8b0f4eddd3
Imanuel Pundoko <ilomon10@gmail.com>
2022-08-23 00:23:00 +0800

    Fix controlButton widget Update package engines

--- src/components/widgets/controlButton.js
+++ src/components/widgets/controlButton.js
@@ -1,133 +1,213 @@
-import React, { useContext, useState, useEffect, useCallback } from 'react';
-import { Button, Classes } from '@blueprintjs/core';
-import { FeathersContext } from 'components/feathers';
-import _uniqBy from 'lodash.uniqby';
-import _merge from 'lodash.merge';
-import { Box } from 'components/utility/grid';
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import { Button, Classes, NonIdealState } from "@blueprintjs/core";
+import { FeathersContext } from "components/feathers";
+import _merge from "lodash.merge";
+import { Box } from "components/utility/grid";
+import { fetchData } from "components/widgets/helper";
+import moment from "moment";
 
 export const buttonOptions = {
   iconName: {
-    type: 'oneOf',
-    options: ['blank', 'asterisk', 'lightbulb', 'dot', 'full-circle']
+    type: "oneOf",
+    options: ["blank", "asterisk", "lightbulb", "dot", "full-circle"],
   },
   direction: {
-    type: 'oneOf',
-    options: ['vertical', 'horizontal']
-  }
-}
+    type: "oneOf",
+    options: ["vertical", "horizontal"],
+  },
+};
 
 export const buttonConfig = {
   maxSeries: 3,
-  acceptedType: ['boolean'],
-  seriesEnabled: true
-}
+  acceptedType: ["boolean"],
+  seriesEnabled: true,
+};
 
 const defaultOptions = {
-  iconName: '',
-  direction: 'vertical'
-}
+  iconName: "",
+  direction: "vertical",
+};
 
-const ControlButton = ({ ...props }) => {
+const ControlButton = ({ onError, ...props }) => {
   const feathers = useContext(FeathersContext);
-  const options = _merge(defaultOptions, props.options)
+  const options = _merge(defaultOptions, props.options);
   const [isLoading, setIsLoading] = useState(false);
   const [series, setSeries] = useState([]);
+  const [error, setError] = useState();
+
+  const onErr = useCallback(
+    (e) => {
+      if (typeof onError === "function") onError(e);
+      setError(e);
+    },
+    [onError]
+  );
+
   useEffect(() => {
     const fetch = async () => {
-      const deviceIds = [..._uniqBy(props.series, 'device').map(v => v.device)];
-      let devices = await feathers.devices.find({
-        query: {
-          _id: { $in: deviceIds },
-          $select: ["_id", 'fields', 'name']
-        }
-      });
-      const query = {
-        deviceId: { $in: deviceIds },
-        $select: ['data', 'deviceId']
+      let query = {
+        $limit: 1,
+      };
+      if (props.timeRange) {
+        query = {
+          ...query,
+          createdAt: {
+            $gte: moment(props.timeRange[0]).toISOString(),
+            $lte: moment(props.timeRange[1]).toISOString(),
+          },
+        };
       }
-      let dataLake = await feathers.dataLake.find({ query });
-      let Series = props.series.map(s => {
-        const device = devices.data.find(d => d._id === s.device);
-        const field = device.fields.find(f => f._id === s.field);
 
-        let data = dataLake.data.filter(dl => (dl.deviceId === device._id));
-        if (typeof data[0] !== 'undefined')
-          data = data[0].data[field.name];
-        else
-          data = undefined;
+      const { dataSources, devices, dataLake } = await fetchData(
+        onErr,
+        feathers,
+        query,
+        props.series
+      );
 
-        return {
-          ...s, data,
-          fieldName: field.name,
-          deviceName: device.name,
-          name: field.name
+      let Series = props.series.map((s) => {
+        let ret = {
+          ...s,
+          data: undefined,
+          fieldName: undefined,
+          deviceName: undefined,
+          name: undefined,
+        };
+        let item;
+        switch (s.type) {
+          case "dataSource":
+            item = dataSources.find((d) => d._id === s.id);
+            break;
+          case "device":
+            item = devices.find((d) => d._id === s.id);
+            break;
+          default:
+            break;
         }
-      })
+
+        const field = item.fields.find((f) => f._id === s.field);
+
+        let data = [];
+        if (s.type === "dataSource") {
+          data = dataLake
+            .filter((dl) => dl.dataSourceId === item._id)
+            .map((dl) => [dl.createdAt, dl.data[field._id]]);
+        }
+
+        if (typeof data[0] !== "undefined") data = data[0].data[field.name];
+        else data = undefined;
+
+        ret.data = data;
+        ret.fieldName = field.name;
+        ret.name = `${field.name} (${item.name})`;
+        ret.sourceName = item.name;
+
+        return ret;
+      });
+
       setSeries([...Series]);
-    }
+    };
     fetch();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [props.timeRange]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     if (props.timeRange) return;
     const onDataCreated = (e) => {
-      setSeries([
-        ...series.map(s => {
+      setSeries((series) => [
+        ...series.map((s) => {
           if (s.device !== e.deviceId) return s;
-          if (typeof e.data[s.fieldName] === 'undefined') return s;
+          if (typeof e.data[s.fieldName] === "undefined") return s;
           s.data = e.data[s.fieldName];
           return s;
-        })
-      ])
-    }
-    feathers.dataLake.on('created', onDataCreated);
+        }),
+      ]);
+    };
+    const onDeviceOutcoming = (e) => {
+      setSeries((series) => [...series.map((s) => {
+        if(s.device !== e._deviceId) return s;
+        if(typeof e[s.fieldName] === "undefined") return s;
+        s.data = e[s.fieldName];
+        return s;
+      })]);
+    };
+    feathers.hub.on("device-outcoming", onDeviceOutcoming);
+    feathers.dataLake.on("created", onDataCreated);
     return () => {
-      feathers.dataLake.removeListener('created', onDataCreated);
-    }
-  }, [series]); // eslint-disable-line react-hooks/exhaustive-deps
+      feathers.hub.removeListener("device-outcoming", onDeviceOutcoming);
+      feathers.dataLake.removeListener("created", onDataCreated);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const onClick = useCallback((cs) => {
-    const push = async () => {
-      setIsLoading(true);
-      let data = {};
-      series.forEach(s => {
-        let res = s.data;
-        if (s.device !== cs.device) return;
-        if (s.fieldName === cs.fieldName) res = !cs.data;
-        data[s.fieldName] = res;
-      });
-      const payload = {
-        deviceId: cs.device,
-        data
-      }
-      try {
-        await feathers.dataLake.create(payload);
-      } catch (e) {
-        console.error(e);
-      }
-      setIsLoading(false);
-    }
-    push();
-  }, [series]);  // eslint-disable-line react-hooks/exhaustive-deps
+  const onClick = useCallback(
+    (cs) => {
+      const push = async () => {
+        setIsLoading(true);
+        const params = {};
+        const data = {
+          [cs.fieldName]: !cs.data,
+        };
+        if (cs.type === "device") {
+          params.device = cs.id;
+        } else if (cs.type === "dataSource") {
+          params.dataSource = cs.id;
+        }
+        try {
+          await feathers.hub.create(data, { query: params });
+        } catch (e) {
+          console.error(e);
+        }
+        setIsLoading(false);
+      };
+      push();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [series]
+  );
+
+  if (error) {
+    return (
+      <NonIdealState
+        icon="graph-remove"
+        title="Error"
+        description={
+          <>
+            <p>{error.message}</p>
+          </>
+        }
+      />
+    );
+  }
 
   return (
-    <Box width={"100%"} height={"100%"} p={2} className={`flex ${options.direction === 'vertical' ? "flex--col" : "flex--row"}`}>
+    <Box
+      width={"100%"}
+      height={"100%"}
+      p={2}
+      className={`flex ${
+        options.direction === "vertical" ? "flex--col" : "flex--row"
+      }`}
+    >
       {series.map((s, i) => (
-        <Button key={i} fill
+        <Button
+          key={i}
+          fill
           className="flex-grow"
           loading={isLoading}
           onClick={() => onClick(s)}
-          active={s.data}
+          active={!!s.data}
           icon={options.iconName}
-          text={(<>
-            <div className={Classes.TEXT_OVERFLOW_ELLIPSIS}>{s.name}</div>
-            <div className={Classes.TEXT_OVERFLOW_ELLIPSIS}>
-              <small>{s.deviceName}</small>
-            </div>
-          </>)} />
+          text={
+            <>
+              <div className={Classes.TEXT_OVERFLOW_ELLIPSIS}>{s.name}</div>
+              <div className={Classes.TEXT_OVERFLOW_ELLIPSIS}>
+                <small>{s.deviceName}</small>
+              </div>
+            </>
+          }
+        />
       ))}
     </Box>
-  )
-}
+  );
+};
 
 export default ControlButton;
